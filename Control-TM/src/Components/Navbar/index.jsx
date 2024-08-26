917539ba479d6f4436c5551ee2c432d2efe11452
JoseI33 <jose.imhoff.20,2@gmail.com>
2024-08-26 17:59:48 -0300

    Creando el navbar

---
+++ Control-TM/src/Components/Navbar/index.jsx
@@ -0,0 +1,60 @@
+import { NavLink } from "react-router-dom";
+const Navbar = () => {
+    const activeStyle = 'underline underline-offset-4'
+
+    return (
+        <nav className="flex justify-between items-center top-0 fixed z-10 w-full py-5 px-8 text-sm font-medium"> 
+            <ul className="flex items-center gap-3">
+                <li className="font-semibold text-lg"> 
+                    <NavLink to='/' className={({isActive}) =>
+                    isActive ? activeStyle : undefined
+                    }>
+                        TMROLDAN SRL
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/equipos' className={({isActive}) =>
+                    isActive ? activeStyle : undefined
+                    }> 
+                        Equipos
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/horometros' className={({isActive}) =>
+                    isActive ? activeStyle : undefined
+                    }>
+                        Horometros
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/repuestos' className={({isActive}) =>
+                    isActive ? activeStyle : undefined
+                    }>
+                        Repuestos
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/torneria' className={({isActive}) =>
+                    isActive ? activeStyle : undefined
+                    }>
+                        Torneria
+                    </NavLink>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    taller@tmroldasrl.com.ar
+                </li>
+                <li>
+                    <NavLink to='/signin' className={({isActive}) =>
+                    isActive ? activeStyle : undefined
+                    }>
+                        Inicia sesión
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
--- Control-TM/src/Pages/App/index.jsx
+++ Control-TM/src/Pages/App/index.jsx
@@ -1,13 +1,13 @@
 import { useRoutes, BrowserRouter } from 'react-router-dom'
 import Home from '../Home'
 import Signin from '../Sigin'
+import Navbar from '../../Components/Navbar'
 import './App.css'
 
 const AppRoutes = () => {
   let routes = useRoutes([
     { path: '/', element: <Home /> },
     { path: '/signin', element: <Signin /> },
-    { path: '*', element: <Notfound /> },
   ])
 
   return routes
